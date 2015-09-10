49a3228d403f0503b90a748d5b505c0a3d2d90ec
Javier Santacruz <jsl@taric.es>
2015-09-10 18:30:53 +0200

    Adds basic package files

---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.dirname(os.path.abspath(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+REQUIREMENTS = open(os.path.join(here, 'requirements.txt')).readlines()
+
+setup(
+    name='upversion',
+    version='0.0.1',
+    description='Release python packages with ease',
+    author='Javier Santacruz',
+    author_email='jsl@taric.es',
+    install_requires=REQUIREMENTS,
+    long_description=README,
+    py_modules=['upversion'],
+    packages=find_packages(),
+    classifiers=[
+        "Internal :: Do not upload"
+    ],
+    entry_points="""
+    [console_scripts]
+    upversion=upversion:cli
+    """
+)
